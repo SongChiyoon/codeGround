bd0975916d74d81f14a3338a471eef39c9881f57
SongChiYun <chiyoonzzang@naver.com>
2017-07-10 14:42:36 +0900

    dp

--- src/fishBread.java
+++ src/fishBread.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
 
 /**
- * Created by songchiyun on 2017. 4. 10..
+ * Created by songchiyun on 2017. 7. 10..
+ *
+ * BAEKJOON 11052번문제
  */
 public class fishBread {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[] dp = new int[1001];
+        int[] p;
         int N = sc.nextInt();
-        int[] p = new int[N+1];
-
-        for(int i=1;i<=N;i++)
+        p = new int[N+1];
+        for(int i=1;i<=N;i++){
             p[i] = sc.nextInt();
-
-        dp[1] = p[1];
-        dp[2] = Math.max(p[2], 2*dp[1]);
-        for(int i = 3; i <= N; i++){
-            if(i % 2 !=0 ){
-                dp[i] = Math.max(p[i], dp[i-1] + dp[1]);
+            if(i%2 ==0){
+                p[i] = Math.max(p[i], p[i/2]*2);
             }
-            else{
-                dp[i] = Math.max(p[i], Math.max(dp[i-2]*2, dp[i-1] + dp[1]));
+        }
+        dp[1] = p[1];
+        for(int i=2;i<=N;i++){
+            dp[i] = p[i];
+            for(int j =1; j <= i/2; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + dp[j]);
             }
         }
         System.out.println(dp[N]);
+
     }
 }
