5465cfb8644c497703bdfd1bb21f324ab2071eef
SongChiYun <chiyoonzzang@naver.com>
2017-09-08 21:39:01 +0900

    baekjoon
    dp

---
+++ src/Tile.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/* Created by songchiyun on 2017. 9. 8..
+ * problem : https://www.acmicpc.net/problem/1904
+ */
+public class Tile {
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[1000001];
+		dp[0] = 0;
+		dp[1] = 0;
+		dp[2] = 2;
+		for(int i=3; i<=N;i++){
+			dp[i-1] %= 15746;
+			dp[i-2] %= 15746;
+			dp[i] = dp[i-1] + dp[i-2];
+		
+		}
+		System.out.println(dp[N]);
+	}
+}
