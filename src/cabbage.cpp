31689753f03f6147264e4a61ebd8cb641a9a3596
SongChiYun <chiyoonzzang@naver.com>
2017-09-29 18:46:57 +0900

    Add files via upload

--- src/cabbage.cpp
+++ src/cabbage.cpp
@@ -16,15 +16,11 @@ int main() {
 
 		cin >> M >> N >> K;
 		for (int k = 0; k < K; k++) {
-
 			int x, y;
 			cin >> x >> y;
 			d[y][x] = 1;
 
 		}
-
-
-
 		int ans = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
@@ -38,30 +34,29 @@ int main() {
 					q.push(c);
 
 					while (!q.empty()) {
-						cout << "gogo" << "\n";
 						Coor a = q.front();
 						q.pop();
 						int x = a.x;
 						int y = a.y;
-						if (x - 1 >= 0 && d[y][x - 1] == 1 && !v[y][x - 1]) {
+						if (x - 1 >= 0)if( d[y][x - 1] == 1 && !v[y][x - 1]) {
 							Coor add;
 							add.x = x - 1; add.y = y;
 							v[y][x - 1] = true;
 							q.push(add);
 						}
-						if (x + 1 < M && d[y][x + 1] == 1 && !v[y][x + 1]) {
+						if (x + 1 < M)if( d[y][x + 1] == 1 && !v[y][x + 1]) {
 							Coor add;
 							add.x = x + 1; add.y = y;
 							v[y][x + 1] = true;
 							q.push(add);
 						}
-						if (y - 1 >= 0 && d[y - 1][x] == 1 && !v[y - 1][x]) {
+						if (y - 1 >= 0)if( d[y - 1][x] == 1 && !v[y - 1][x]) {
 							Coor add;
 							add.x = x; add.y = y - 1;
 							v[y - 1][x] = true;
 							q.push(add);
 						}
-						if (y + 1 < N && d[y + 1][x] == 1 && !v[y + 1][x]) {
+						if (y + 1 < N)if(d[y + 1][x] == 1 && !v[y + 1][x]) {
 							Coor add;
 							add.x = x; add.y = y + 1;
 							v[y + 1][x] = true;
@@ -76,8 +71,11 @@ int main() {
 			}
 
 		}
+        
 		cout << ans << "\n";
+        
 		for(int i=0;i<N;i++)
+            
 			for (int j = 0; j < M; j++) {
 				d[i][j] = 0;
 				v[i][j] = false;
